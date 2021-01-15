919089ec4f31126a3bfd764c1f25361bbe36d65e
enfieldcat <36611472+enfieldcat@users.noreply.github.com>
2021-01-16 09:40:24 +1300

    Update photoArchive.sh
    Exit if destination dir does not exist, and skip sources which are not directories.

--- photoArchive.sh
+++ photoArchive.sh
@@ -1,3 +1,4 @@
+$ cat photo.sh
 #!/bin/bash
 #
 # PhotoArchive.sh
@@ -18,28 +19,39 @@ sources="/cygdrive/c/users"
 # Define where the photos should be stored, on my system /cygdrive/g is my USB drive/
 dest="/cygdrive/g"
 
+# Exit if destination directory does not exist
+if [ ! -d "${dest}" ] ; then
+  echo "${dest} is missing. Insert USB drive and create directory"
+  exit 1
+fi
+
 # Work through all sources listed
 echo "${sources}" | while read SRC ; do
-  echo "Searching ${SRC}"
-  # These are the file types we will include in our archive.
-  # Additional file suffixes can be added to this list
-  for filetype in jpg JPG JPEG jpeg ; do
-    echo " - ${filetype} files"
-    # Process through a list of candidate files, but exclude trash and application data
-    find "${SRC}" -name "*.${filetype}" -type f -print 2>/dev/null | egrep -v ".Trash|AppData" | while read FILE ; do
-      # echo "   + ${FILE}"
-      # First check exif data in the file header as the authoritative date taken information
-      ds=`exif "${FILE}" 2>/dev/null | grep "^Date" | head -1 | awk -F '|' ' { print $2 } ' | awk -F ':' ' { print $1 "/" $2 }'`
-      if [ "${ds}" = "" ] ; then
-        # If not found then use the file date as the date reference
-        ds=`ls --full-time "${FILE}" | awk ' { split ($6, part, "-"); print part[1] "/" part[2] } '`
-      fi
+  if [ -d "${SRC}" ] ; then
+    echo "Searching ${SRC}"
+    # These are the file types we will include in our archive.
+    # Additional file suffixes can be added to this list
+    for filetype in jpg JPG JPEG jpeg ; do
+      echo " - ${filetype} files"
+      # Process through a list of candidate files, but exclude trash and application data
+      find "${SRC}" -name "*.${filetype}" -type f -print 2>/dev/null | egrep -v ".Trash|AppData" | while read FILE ; do
+        # echo "   + ${FILE}"
+        # First check exif data in the file header as the authoritative date taken information
+        ds=`exif "${FILE}" 2>/dev/null | grep "^Date" | head -1 | awk -F '|' ' { print $2 } ' | awk -F ':' ' { print $1 "/" $2 }'`
+        if [ "${ds}" = "" ] ; then
+          # If not found then use the file date as the date reference
+          ds=`ls --full-time "${FILE}" | awk ' { split ($6, part, "-"); print part[1] "/" part[2] } '`
+        fi
       # Create the destination directory if it is missing
-      if [ ! -d "${dest}/${ds}" ] ; then
-        mkdir -p "${dest}/${ds}"
-      fi
-      # Copy the file over to the destination
-      rsync -p -t "${FILE}" "${dest}/${ds}"
+        if [ ! -d "${dest}/${ds}" ] ; then
+          mkdir -p "${dest}/${ds}"
+        fi
+        # Copy the file over to the destination
+        rsync -p -t "${FILE}" "${dest}/${ds}"
+      done
     done
-  done
+  else
+    echo "Skipping ${SRC} - not a directory"
+  fi
 done
+exit 0
